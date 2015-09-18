7a7da18900e4b9b03973646ae12a505c6d88904a
zhaozhiming <kingzzm1982@sina.com>
2015-09-18 14:35:35 +0800

    add angular

--- simple-todos-angular.js
+++ simple-todos-angular.js
@@ -1,23 +1,13 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+  angular.module('simple-todos',['angular-meteor']);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+  angular.module('simple-todos').controller('TodosListCtrl', ['$scope',
+    function ($scope) {
+      $scope.tasks = [
+        { text: 'This is task 1' },
+        { text: 'This is task 2' },
+        { text: 'This is task 3' }
+      ];
+      }]);
 }
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
